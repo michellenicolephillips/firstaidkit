e4fb18287a46dc620ebbfd7b5e3b816a86a799a4
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2022-03-17 11:47:41 -0500

    updated css import on form.tsx

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import styles from '../styles/globals.css';
+import '../styles/globals.css';
 
 function NewApplicantForm() {
      const [applicantName, setApplicantName] = useState('');
